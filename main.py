07afbd3b57003415a2d541596f3c8e873ab1dff7
Devsharma431 <devpython345898@gmail.com>
2023-09-27 18:07:18 +0530

    pallindrom case and condition work

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-f = input("ENter ihfevh : " )
-print(f)
+f = int(input("Enter ur no. :"))
+f = str(f)
+d = f[::-1]
+if d == f :
+    print("THis no. is pallindrom")
+else:
+    print("This is not a pallindrom ")
