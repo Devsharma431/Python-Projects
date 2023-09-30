7ef568f30f879fd8ce4d1600b4096d42119c4e90
Devsharma431 <devpython345898@gmail.com>
2023-09-30 20:14:52 +0530

    Working on fucntions

--- functions.py
+++ functions.py
@@ -1,4 +1,18 @@
+f = int(input("Enter the number : "))
+g = int(input("echo the number : " ))
 def sun(a,b):
     return a + b
-f = sun(2,3)
+def dun(a,b):
+    return a - b
+def mun(a,b):
+    return a * b
+def gun(a,b):
+    return a / b
+f = sun(f,g)
+fd = dun(f, g)
+fs = mun(f, g)
+fg = gun(f, g)
 print(f)
+print(fd)
+print(fs)
+print(fg)
