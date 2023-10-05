3fb7f0ccadb1ba77c02551970bf0106e0fc48691
Devsharma431 <devpython345898@gmail.com>
2023-10-05 21:45:39 +0530

    factorial python file

---
+++ factorial.py
@@ -0,0 +1,12 @@
+# import math
+# def factorial(n):
+#     return(math.factorial(n))
+# num = int(input("enter number : "))
+# print(factorial(num))
+# Python 3 program to find 
+# factorial of given number
+
+def factorial(n):
+	return 1 if (n==1 or n==0) else n * factorial(n - 1) 
+num = 5
+print("Factorial of",num,"is",factorial(num))
