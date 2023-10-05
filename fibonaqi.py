e1667b912bf19469b58401eaf078712aa58ea17c
Devsharma431 <devpython345898@gmail.com>
2023-10-05 21:33:32 +0530

    some changs;

--- fibonaqi.py
+++ fibonaqi.py
@@ -1,18 +1,17 @@
-# def fibonaqi(n):
-#     if n == 0:
-#         return 0
-#     elif n == 1:
-#         return 1
-#     else:
-#         return fibonaqi(n-1) + fibonaqi(n-2)
-# n = int(input("enter a no. :"))
-# print(fibonaqi(n))
+def fibonaqi(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonaqi(n-1) + fibonaqi(n-2)
+n = int(input("enter a no. :"))
+print(fibonaqi(n))
 n = int(input("enter a no. :"))
 num1 = 0
 num2 = 1
 next_number = num2
-count = 10
-
+count = 0
 while True:
 	if count <= n:
 		print("Invalid number")
