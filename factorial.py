2cfe68a2fdeca769bd3faf49bd346fe1d9eb2922
Devsharma431 <devpython345898@gmail.com>
2023-10-05 21:56:10 +0530

    day of 12

--- factorial.py
+++ factorial.py
@@ -7,6 +7,6 @@
 # factorial of given number
 
 def factorial(n):
-	return 1 if (n==1 or n==0) else n * factorial(n - 1) 
+	return 2 if (n==1 or n==0) else n * factorial(n - 1) 
 num = 5
 print("Factorial of",num,"is",factorial(num))
