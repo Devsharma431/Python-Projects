dac8c4050cc7706dbecd0a75ea9977f266f23f12
Devsharma431 <devpython345898@gmail.com>
2023-10-08 17:30:15 +0530

    fibonacci done

--- fibonaqi.py
+++ fibonaqi.py
@@ -1,17 +1,19 @@
-# d = int(input("enter ur n. : "))
-# a , b = 0,1
-# sum =0
-# for i in range(0,d):
-#     a = b
-#     b = sum
-#     sum = a+b
-#     print(sum, end='')
-def fast(n):
+# # FIBONACCI BY FOR LOOP
+n = int(input("enter a no : "))
+a,b = 0,1
+sum = 0
+for i in range(0,n):
+    print(sum, end= "\n")
+    a = b
+    b = sum
+    sum = a + b
+# FIBONACCI BY RECURSION
+def rec(n):
     if n == 0:
         return 0
     elif n == 1:
         return 1
     else:
-        return fast(n+1) + fast(n-2)
-f = int(input('enter a no : '))
-print(fast(f))
+        return rec(n-1) + rec(n-2)
+f = int(input('enter a no :'))
+print(rec(f))
