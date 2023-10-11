0beb45494062747373018766c10a22936ae90e33
Devsharma431 <devpython345898@gmail.com>
2023-10-11 17:53:22 +0530

    updates

--- file_handling.py
+++ file_handling.py
@@ -1,62 +1,10 @@
 # # Python code to create a file
-# file = open('py.txt','w')
-# file.write("This is the write command")
-# file.write("It allows us to write in a particular file")
-# file.write("THis is a new text by dev sharma ")
-# file.close()
-# f = open('py.txt','r')
-# print(f.read())
-import os
-
-def create_file(filename):
-	try:
-		with open(filename, 'w') as f:
-			f.write('Hello, world!\n')
-		print("File " + filename + " created successfully.")
-	except IOError:
-		print("Error: could not create file " + filename)
-
-def read_file(filename):
-	try:
-		with open(filename, 'r') as f:
-			contents = f.read()
-			print(contents)
-	except IOError:
-		print("Error: could not read file " + filename)
-
-def append_file(filename, text):
-	try:
-		with open(filename, 'a') as f:
-			f.write(text)
-		print("Text appended to file " + filename + " successfully.")
-	except IOError:
-		print("Error: could not append to file " + filename)
-
-def rename_file(filename, new_filename):
-	try:
-		os.rename(filename, new_filename)
-		print("File " + filename + " renamed to " + new_filename + " successfully.")
-	except IOError:
-		print("Error: could not rename file " + filename)
-
-def delete_file(filename):
-	try:
-		os.remove(filename)
-		print("File " + filename + " deleted successfully.")
-	except IOError:
-		print("Error: could not delete file " + filename)
-
-
-if __name__ == '__main__':
-	filename = "example.txt"
-	new_filename = "new_example.txt"
-
-	create_file(filename)
-	read_file(filename)
-	append_file(filename, "This is some additional text.\n")
-	read_file(filename)
-	rename_file(filename, new_filename)
-	read_file(new_filename)
-	delete_file(new_filename)
-
-
+file = open('py.txt','w')
+file.write("This is the write command")
+file.write("It allows us to write in a particular file")
+file.write("THis is a new text by dev sharma end = '\n'")
+d = input("Enter ur message : ")
+file.write()
+file.close()
+f = open('py.txt','r')
+print(f.read())
--- loop.py
+++ loop.py
@@ -1,23 +1,27 @@
-count = 0
-while( count < 8):
-    count = count + 1
-    print("Hello worldS")
-n = int(input("enter a no : "))
-# 1 to 100 program
-for i in range(0, n):
-    print(i, end= "\n")
+# count = 0
+# while( count < 8):
+#     count = count + 1
+#     print("Hello worldS")
+# n = int(input("enter a no : "))
+# # 1 to 100 program
+# for i in range(0, n):
+#     print(i, end= "\n")
 #  100 to  1 program
-print("-----------------------------------------------")
+# print("-----------------------------------------------")
 # for i in range(n,0,-1):
 #     print(i, end= "\n")
 # # 
-# Initialize a variable to store the sum
-sum_of_natural_numbers = 0
-
-# Use a loop to add the first 10 natural numbers
-for i in range(1, n):
-    sum_of_natural_numbers += i
-
-# Print the sum
-print("The sum of the first 10 natural numbers is:", sum_of_natural_numbers)
-
+# sum_of_natural_numbers = 0
+# for i in range(1, n):
+#     sum_of_natural_numbers += i
+# print("The sum of the first 10 natural numbers is:", sum_of_natural_numbers)
+import math 
+def s(n):
+    return math.factorial(n)
+j = int(input("enter a no : "))
+print(s(j))
+f = 0
+for i in range(0, j):
+    f = j * 1
+    f -=  1
+    print(f, end= "\n")
