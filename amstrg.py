b44269174da7d00092dde8c1f61911cf8c637465
Devsharma431 <devpython345898@gmail.com>
2023-10-15 12:57:52 +0530

    learning notes of basic linux commands

---
+++ amstrg.py
@@ -0,0 +1,9 @@
+sum = 0
+for i in range(1,11):
+    i = i**2
+    sum += i 
+    print(sum)
+    # print(d)
+# sum_of_squares = sum([x ** 2 for x in range(1, 11)])
+
+# print("The sum of the squares of the first 10 natural numbers is:", sum_of_squares)
