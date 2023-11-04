7d6bef53657078faada669ac7634d335b5bd3a82
Devsharma431 <devpython345898@gmail.com>
2023-11-04 22:05:45 +0530

    files

--- nestedlooops.py
+++ nestedlooops.py
@@ -1,4 +1,8 @@
-import pyttsx3
-d = pyttsx3.init()
-d.say("Hello world")
-d.runAndWait()
+for i in range(5):
+    for j in range(i +1):
+        print("&", end=" ")
+    print()
+for i in range(5):
+    for j in range(i-1):
+        print('*', end=" ")
+    print()
